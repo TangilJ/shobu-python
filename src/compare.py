164f3603fbea956f5d023db407f2f326586db11d
Tangil <21197843+TheBlocks@users.noreply.github.com>
2024-04-27 17:25:51 +0100

    Account for edge cases in Compare

--- src/compare.py
+++ src/compare.py
@@ -1,13 +1,13 @@
 from enum import Enum, auto
 
 from src import engine
-from src.mcts import AlphaZeroMCTS
 from src.player import Player
 
 
 class Winner(Enum):
     Player1 = auto()
     Player2 = auto()
+    Loop = auto()
 
 
 class Compare:
@@ -19,20 +19,37 @@ class Compare:
         state = engine.start_state
         player = 1
 
+        iters = 0
         while state.win == engine.Win.GameOngoing:
+            if len(engine.get_next_states(state.board)) == 0:
+                break
+
             if player == 1:
                 state = self.player1.move(state)
             else:
                 state = self.player2.move(state)
             player *= -1
 
+            iters += 1
+            if iters > 400:
+                # If the game goes on for too long, it's a loop
+                return Winner.Loop
+
         win_multiplier = 1 if state.win == engine.Win.OwnWin else -1
         return Winner.Player1 if player * win_multiplier == 1 else Winner.Player2
 
-    def evaluate(self, num_games: int) -> float:
+    def evaluate(self, num_games: int) -> (int, int, int):
         p1_wins = 0
+        p2_wins = 0
+        loops = 0
+
         for _ in range(num_games):
             winner = self._play()
             if winner == Winner.Player1:
                 p1_wins += 1
-        return p1_wins / num_games
+            elif winner == Winner.Player2:
+                p2_wins += 1
+            else:
+                loops += 1
+
+        return p1_wins, p2_wins, loops
